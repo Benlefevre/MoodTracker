db45f537c79ae2cd6f364fc714138d59882920a5
Benoit Lefèvre <speedben94@hotmail.fr>
2019-01-13 18:10:09 +0100

    Add Gson dependency ('com.google.code.gson:gson:2.8.5') in build.gradle. Create createDialog() that is call in mHistoricalButton listener. Create saveMood() that parse the current date in json String in MoodActivity OnPause() and initMood() that parse a String json in a Mood object. Create changeMood() that change the layout background and the imageview according to the current Mood.

--- app/src/main/java/com/benoitlefevre/moodtracker/controller/MoodActivity.java
+++ app/src/main/java/com/benoitlefevre/moodtracker/controller/MoodActivity.java
@@ -1,18 +1,26 @@
 package com.benoitlefevre.moodtracker.controller;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.benoitlefevre.moodtracker.R;
 import com.benoitlefevre.moodtracker.model.Mood;
+import com.google.gson.Gson;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class MoodActivity extends AppCompatActivity {
 
@@ -23,6 +31,9 @@ public class MoodActivity extends AppCompatActivity {
     private SharedPreferences mPreferences;
     private Date mDate;
     private Mood[] mMoods;
+    private Mood mCurrentMood;
+    private Gson mGson;
+    private String today;
 
 
     @Override
@@ -32,10 +43,12 @@ public class MoodActivity extends AppCompatActivity {
 
         initActivity();
         mMoods = createMoods();
+        initMood();
 
         mCommentaryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                createDialog();
             }
         });
 
@@ -56,6 +69,10 @@ public class MoodActivity extends AppCompatActivity {
         mCommentaryButton = findViewById(R.id.MoodActivity_commentary_btn);
         mHistoricalButton = findViewById(R.id.MoodActivity_history_btn);
         mDate = new Date();
+        mGson = new Gson();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("E-w",Locale.getDefault());
+        today = dateFormat.format(mDate);
+
     }
 
     public Mood[] createMoods(){
@@ -67,5 +84,52 @@ public class MoodActivity extends AppCompatActivity {
         return new Mood[]{mSad,mDisappointed,mSimple,mHappy,mSuperHappy};
     }
 
+    public void initMood(){
+        String json = mPreferences.getString(today,null);
+        if(json != null)
+            mCurrentMood = mGson.fromJson(json,Mood.class);
+        else
+            mCurrentMood = mMoods[2];
+        changeMood();
+    }
 
+    public void changeMood(){
+        mLayout.setBackgroundResource(mCurrentMood.getColor());
+        mImageView.setImageResource(mCurrentMood.getImage());
+    }
+
+    public void createDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MoodActivity.this);
+        LayoutInflater inflater = MoodActivity.this.getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.alertcommentary,null));
+        builder.setTitle("Comment");
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Dialog diaCom = (Dialog) dialog;
+                EditText comDialog = diaCom.findViewById(R.id.alertcommentary_edittext);
+                mCurrentMood.setCommentary(comDialog.getText().toString());
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        })
+                .setCancelable(false)
+                .create()
+                .show();
+    }
+
+    public void saveMood(){
+        String json = mGson.toJson(mCurrentMood);
+        mPreferences.edit().putString(today,json).apply();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveMood();
+    }
 }
