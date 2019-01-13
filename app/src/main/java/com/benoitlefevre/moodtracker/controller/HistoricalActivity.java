1e0f7e2e48680c355cb7f6bb22492d9e66621c53
Benoit Lefèvre <lefevre.benoit75@gmail.com>
2019-01-13 21:55:36 +0100

    Create initActivity() that initializes all HistoricalActivity's fields. Create initMood() that get in a Map all SharedPreferences and convert Map's values in Mood object in an ArrayList<Mood> with Gson. ArrayList is sort by descending order by the field date of Mood object, remove the lastIndex and create a sublist to keep only 7 items in ListView.

--- app/src/main/java/com/benoitlefevre/moodtracker/controller/HistoricalActivity.java
+++ app/src/main/java/com/benoitlefevre/moodtracker/controller/HistoricalActivity.java
@@ -1,15 +1,59 @@
 package com.benoitlefevre.moodtracker.controller;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.benoitlefevre.moodtracker.R;
+import com.benoitlefevre.moodtracker.model.Mood;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 
 public class HistoricalActivity extends AppCompatActivity {
+
+    private SharedPreferences mPreferences;
+    private ListView mListView;
+    private List<Mood> mMoodList;
+    private int mHeightScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historical);
+
+        initActivity();
+        initMoods();
+    }
+
+    public void initActivity(){
+        mListView = findViewById(R.id.HistoricalActivity_listView);
+        mPreferences = getSharedPreferences("MoodSave",MODE_PRIVATE);
+        mMoodList = new ArrayList<>();
+    }
+
+    public void initMoods(){
+        Gson gson = new Gson();
+        Map<String, ?> prefList = mPreferences.getAll();
+        for (Map.Entry<String,?>  entry : prefList.entrySet()) {
+            String json = entry.getValue().toString();
+            Mood mMood = gson.fromJson(json,Mood.class);
+            mMoodList.add(mMood);
+        }
+        Collections.sort(mMoodList, new Comparator<Mood>() {
+            @Override
+            public int compare(Mood o1, Mood o2) {
+                return o1.getDay().compareTo(o2.getDay());
+            }
+        });
+        mMoodList.remove(mMoodList.size()-1);
+        if(mMoodList.size()>= 7) {
+            mMoodList = mMoodList.subList(mMoodList.size()-7,mMoodList.size());
+        }
     }
 }
