98d278b313ee4f8810534457112b08263e6b4a03
Benoit Lefèvre <lefevre.benoit75@gmail.com>
2019-01-15 15:02:43 +0100

    Add a new Dependency in build.gradle 'com.github.PhilJay:MPAndroidChart:v3.1.0-alpha' It's an open-source library to display easily data in chart. Create a new StatActivity.java and its layout activity_stat.xml. Add in this layout a view PieChart.

---
+++ app/src/main/java/com/benoitlefevre/moodtracker/controller/StatActivity.java
@@ -0,0 +1,15 @@
+package com.benoitlefevre.moodtracker.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.benoitlefevre.moodtracker.R;
+
+public class StatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stat);
+    }
+}
